c1a68605d66cdce711c8c48eb78b65ca5a577832
Chul <r.chulapat@gmail.com>
2017-08-14 01:29:09 +0700

    Hi Mom

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Cache Matrix - R Programming Week 3 - Assignment 2 - Lexical Scoping 
+# English Is Not My Mother Tounge; Sorry If there are any mistake.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL #Cache Holder
+        #Clear the Cache
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x #Save Matrix For Functions To call
+        setinv <- function(inv) m <<- inv #Function To Call For Caching Inv. Matrix
+        getinv <- function() m #Inverse Matrix For Functions To Call
+        #Naming FUnctions (get,setinv,getinv) to call later
+        list(
+                set = set,
+                get = get,
+                setinv = setinv,
+                getinv = getinv
+        )
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv() #Get Inverse Matrix From Cache Function
+        #Check If there is Inv. Matrix In the Cache Function
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        #If There Is no Cache / Calculate Inverse Matrix
+        data <- x$get() #Get Matrix / Ready for a Calculation
+        inv <- solve(data, ...) #Calculate Inverse Matrix
+        x$setinv(inv) #Take This Function's "inv" and Cache it
+        inv #Show Inverse Matrix
 }
